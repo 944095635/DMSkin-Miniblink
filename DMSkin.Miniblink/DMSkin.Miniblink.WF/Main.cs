f6d7e17cf6a22470d8cd2efb67f2e5faca924dba
Dream.Machine <944095635@qq.com>
2018-12-13 14:46:24 +0800

    提交代码

---
+++ DMSkin.Miniblink/DMSkin.Miniblink.WF/Main.cs
@@ -0,0 +1,30 @@
+﻿using Kyozy.MiniblinkNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DMSkin.Miniblink.WF
+{
+    public partial class Main : Form
+    {
+        private WebView m_wke;
+        public Main()
+        {
+            InitializeComponent();
+
+            m_wke = new WebView(); //构造函数可以是无参，但是后面必须调用 Bind 方法
+            m_wke.Bind(this);//随便绑定一个控件或窗口
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            m_wke.LoadURL("http://www.baidu.com");
+        }
+    }
+}
---
+++ DMSkin.Miniblink/DMSkin.Miniblink/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Kyozy.MiniblinkNet;
+using System.Windows;
+
+namespace DMSkin.Miniblink
+{
+    public partial class MainWindow : Window
+    {
+        private WebView m_wke;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            m_wke = new WebView(); //构造函数可以是无参，但是后面必须调用 Bind 方法
+            m_wke.Bind(pictureBox);//随便绑定一个控件或窗口
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            m_wke.LoadURL("http://www.baidu.com");
+        }
+    }
+}
